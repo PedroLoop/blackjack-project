07b3c5c7945c0e109c230a25b7c08770f73428eb
PedroLoop <104634468+PedroLoop@users.noreply.github.com>
2022-06-13 16:52:05 +0200

    Tous les fichiers

---
+++ script/script.js
@@ -0,0 +1,31 @@
+
+
+class Gameboard{
+    constructor(){   
+
+    }
+
+    ShowDiv(){
+        document.getElementById('buttonBox').style.display = "";
+    }
+    // function
+}
+
+class Cards{
+    constructor(){   
+
+    }
+
+    shuffle(){
+
+    }
+
+    showDeck(){
+
+    }
+    
+    getCard(numberOfCard){
+
+    }
+    // function
+}
